28af0de0827f74f649f564da0908ba21d7d924b7
zpdl95 <won8070@naver.com>
2024-02-28 18:19:16 +0900

    about page update

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,88 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
+import React from 'react';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
-import { urlFor } from '../../api/sanity';
-import { useSanityContext } from '../../context/SanityContext';
+import githubImg from '../../assets/github.png';
 
 const About = () => {
-  const { aboutData, expData } = useSanityContext();
-  const [sortedExpData, setSortedExpData] = useState();
-
-  useEffect(() => {
-    if (expData) {
-      const sortedData = expData.toSorted((a, b) => {
-        if (a._createdAt < b._createdAt) {
-          return -1;
-        } else if (a._createdAt > b._createdAt) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
-
-      setSortedExpData(sortedData);
-    }
-  }, [expData]);
-
   return (
     <>
       <h2 className='head-text'>
         <span>About</span>
       </h2>
       <div className='app__about-wrapper'>
-        <div className='app__profiles'>
-          {aboutData?.map((about, index) => (
-            <div key={about.title + index} className='app__profile-item'>
-              <div className='app__profile-img'>
-                <img src={urlFor(about.imgUrl)} alt={about.title} />
-              </div>
-
-              <div className='app__profile-desc'>
-                <p className='p-text' style={{ marginTop: 10 }}>
-                  {about.description}
-                </p>
-              </div>
-            </div>
-          ))}
+        <div className='app__about-card'>
+          <h3 className='title'>me</h3>
+          <p>최재원</p>
+          <p>1995.06.03</p>
+          <p>경남대학교 심리학과</p>
         </div>
-
-        <div className='app__exp'>
-          {sortedExpData?.map((experience) => (
-            <motion.div className='app__exp-item' key={experience.year}>
-              <div className='app__exp-year'>
-                <p className='bold-text'>{experience.year}</p>
-              </div>
-              <motion.div className='app__exp-works'>
-                {experience.works.map((work, index) => (
-                  <>
-                    <motion.div
-                      whileInView={{ opacity: [0, 1] }}
-                      transition={{ duration: 0.5 }}
-                      className='app__exp-work'
-                      data-tip
-                      data-for={work.name + index}
-                      key={work.name}
-                    >
-                      <h4 className='bold-text'>{work.name}</h4>
-                      <p className='p-text'>{work.company}</p>
-                    </motion.div>
-                    {work.desc && (
-                      <ReactTooltip
-                        id={work.name + index}
-                        effect='solid'
-                        arrowColor='#fff'
-                        className='tooltip'
-                      >
-                        {work.desc}
-                      </ReactTooltip>
-                    )}
-                  </>
-                ))}
-              </motion.div>
-            </motion.div>
-          ))}
+        <div className='app__about-card'>
+          <h3 className='title'>education</h3>
+          <div>
+            <p className='term'>2019.06.21~2019.09.06</p>
+            <p>데이터 사이언스 및 AI 융합 기반의 빅리더</p>
+          </div>
+          <div>
+            <p className='term'>2020.08~</p>
+            <p>노마드 코더</p>
+          </div>
+          <div>
+            <p className='term'>2022.12~</p>
+            <p>드림코딩 아카데미</p>
+          </div>
+        </div>
+        <div className='app__about-card'>
+          <h3 className='title'>channel</h3>
+          <div className='channel-info'>
+            <img src={githubImg} alt='github' />
+            <a href='https://github.com/zpdl95/' target='_blank'>
+              https://github.com/zpdl95/
+            </a>
+          </div>
         </div>
       </div>
     </>
